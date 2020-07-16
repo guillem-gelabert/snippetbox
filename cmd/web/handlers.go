dd8d65ff97f39406633a0dee20c9590b2e05023e
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-16 15:10:15 +0200

    F add error handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,17 +21,17 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		app.errorLog.Println(err.Error())
-		http.Error(w, "Interal Server Error", 500)
+		app.serverError(w, err)
 	}
 	if err := t.Execute(w, nil); err != nil {
-		http.Error(w, "Interal Server Error", 500)
+		app.serverError(w, err)
 	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -41,7 +41,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
