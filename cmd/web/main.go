923bedd53552475a0af0065b97b3d72630a88b63
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-16 14:28:48 +0200

    F customize logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.Int("addr", 4000, "HTTP network address")
 	static := flag.String("static-dir", "./ui/static", "Static files directory")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/snippet", showSnippet)
@@ -20,7 +24,14 @@ func main() {
 	fileServer := http.FileServer(http.Dir(*static))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on port", *addr)
-	err := http.ListenAndServe(fmt.Sprint(":", *addr), mux)
-	log.Fatal(err)
+	infoLog.Println("Starting server on port", *addr)
+
+	srv := &http.Server{
+		Addr:     fmt.Sprint(":", *addr),
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal("ERROR", err)
 }
