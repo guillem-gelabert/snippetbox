29bc0a2f64a7ce8fad7c38ffb42c34a4b7cc9cdf
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-16 14:39:09 +0200

    F pass loggers through DI

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -21,7 +20,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	t, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Interal Server Error", 500)
 	}
 	if err := t.Execute(w, nil); err != nil {
@@ -29,7 +28,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -39,7 +38,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display specific snippet #%d", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", 405)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,30 +8,36 @@ import (
 	"os"
 )
 
+type application struct {
+	infoLog, errorLog *log.Logger
+}
+
 func main() {
 	addr := flag.Int("addr", 4000, "HTTP network address")
 	static := flag.String("static-dir", "./ui/static", "Static files directory")
 	flag.Parse()
 
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+	mux.HandleFunc("/", app.home)
 
 	fileServer := http.FileServer(http.Dir(*static))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	infoLog.Println("Starting server on port", *addr)
+	app.infoLog.Println("Starting server on port", *addr)
 
 	srv := &http.Server{
 		Addr:     fmt.Sprint(":", *addr),
-		ErrorLog: errorLog,
+		ErrorLog: app.errorLog,
 		Handler:  mux,
 	}
 
 	err := srv.ListenAndServe()
-	errorLog.Fatal("ERROR", err)
+	app.errorLog.Fatal("ERROR", err)
 }
