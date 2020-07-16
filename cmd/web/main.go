277ce63fe802ed44e6e1c1922ce5f1c2a553bc9f
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-16 13:54:29 +0200

    F extract port and static-dir to args

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.Int("addr", 4000, "HTTP network address")
+	static := flag.String("static-dir", "./ui/static", "Static files directory")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.HandleFunc("/", home)
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir(*static))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Starting server on port", *addr)
+	err := http.ListenAndServe(fmt.Sprint(":", *addr), mux)
 	log.Fatal(err)
 }
