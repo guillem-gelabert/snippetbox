d88164ecfd88652b5156cff62ccf2d80d17e2bfc
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-20 21:15:52 +0200

    F implement Insert method on the snippet model

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -13,7 +13,21 @@ type SnippetModel struct {
 
 // Insert insdrts a new snippet into the database.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `
+		INSERT INTO snippets (title, content, created, expires)
+		VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(),INTERVAL ? DAY))
+	`
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 // Get returns a specific snipped based on its id.
