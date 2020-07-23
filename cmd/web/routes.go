1a792620aa5205d8110909e5c1ebfaec7dc637d3
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-23 14:00:48 +0200

    F replace manuall middleware composition with alice

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes(static *string) http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
@@ -11,5 +16,5 @@ func (app *application) routes(static *string) http.Handler {
 	fileServer := http.FileServer(http.Dir(*static))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
