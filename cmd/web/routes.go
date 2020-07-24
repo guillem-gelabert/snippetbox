78f52ebee69eea84d3e223632b3e5f5562d33491
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-24 15:52:41 +0200

    B pass home handler through session middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes(static *string) http.Handler {
 
 	mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
 	mux.Get("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm))
 	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
 	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
