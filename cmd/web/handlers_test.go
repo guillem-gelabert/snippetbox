b6108612eed4b2e529fe9da5b60795d015b31d4e
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-26 12:24:25 +0200

    t use test server abstraction

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,38 +1,22 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(ioutil.Discard, "", 0),
-		infoLog:  log.New(ioutil.Discard, "", 0),
-	}
-
 	st := "./ui/static"
 	var static *string = &st
 
-	ts := httptest.NewTLSServer(app.routes(static))
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes(static))
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("expected %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-	body, err := ioutil.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
+	if code != http.StatusOK {
+		t.Errorf("expected %d; got %d", http.StatusOK, code)
 	}
 
 	if string(body) != "OK" {
