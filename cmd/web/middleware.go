282cbbc1b0b2e7af921f39f1d216cb8bbfe9bca9
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-26 11:46:13 +0200

    t test secure headers middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -50,7 +50,7 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-XSS-Protection", "1;mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
 
 		next.ServeHTTP(w, r)
---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSecureHeaders(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	secureHeaders(next).ServeHTTP(rr, r)
+
+	rs := rr.Result()
+
+	frameOptions := rs.Header.Get("X-Frame-Options")
+	if frameOptions != "deny" {
+		t.Errorf("expected %q; got %q", "deny", frameOptions)
+	}
+
+	xssProtection := rs.Header.Get("X-XSS-Protection")
+	if xssProtection != "1;mode=block" {
+		t.Errorf("expected %q; got %q", "1;mode=block", xssProtection)
+	}
+
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("expected body to be %q", "OK")
+	}
+}
