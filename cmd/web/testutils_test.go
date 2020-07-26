109cf85c40325e30f8cd92e8e0b9537acfa63a2f
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-26 13:27:03 +0200

    t  use dependency injection to mock models, template and session

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -7,12 +7,29 @@ import (
 	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/golangcollege/sessions"
+	"github.com/guillem-gelabert/snippetbox/pkg/models/mock"
 )
 
 func newTestApplication(t *testing.T) *application {
+	templateCache, err := newTemplateCache("../../../ui/html/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	session := sessions.New([]byte("af*P.Z8Q7.TpGsNrtqvEdK3bNNfz6cCu"))
+	session.Lifetime = 12 * time.Hour
+	session.Secure = true
+
 	return &application{
-		errorLog: log.New(ioutil.Discard, "", 0),
-		infoLog:  log.New(ioutil.Discard, "", 0),
+		errorLog:      log.New(ioutil.Discard, "", 0),
+		infoLog:       log.New(ioutil.Discard, "", 0),
+		session:       session,
+		snippets:      &mock.SnippetModel{},
+		templateCache: templateCache,
+		users:         &mock.UserModel{},
 	}
 }
 
