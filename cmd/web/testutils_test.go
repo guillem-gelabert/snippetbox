6f6e0dd2e08c2cc003ad3cb6e10619c364693820
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-26 15:01:05 +0200

    t mock CSRF behaviour

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 	"time"
 
@@ -13,8 +15,19 @@ import (
 	"github.com/guillem-gelabert/snippetbox/pkg/models/mock"
 )
 
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
+
 func newTestApplication(t *testing.T) *application {
-	templateCache, err := newTemplateCache("../../../ui/html/")
+	templateCache, err := newTemplateCache("./../../ui/html/")
 	if err != nil {
 		t.Fatal(err)
 	}
