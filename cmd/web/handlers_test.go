8a6ca3ff4701bbad8e7773f63b00eeb5bbcb9fa2
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-26 14:14:58 +0200

    t show snippet endpoint

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	st := "./ui/static"
+	st := "./../../ui/static"
 	var static *string = &st
 
 	app := newTestApplication(t)
@@ -21,5 +22,39 @@ func TestPing(t *testing.T) {
 
 	if string(body) != "OK" {
 		t.Errorf("Expected body to equal %q", "OK")
+	}
+}
+
+func TestShowSnippet(t *testing.T) {
+	st := "./ui/static"
+	var static *string = &st
+
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes(static))
+	defer ts.Close()
+
+	testCases := []struct {
+		desc         string
+		urlPath      string
+		expectedCode int
+		expectedBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-Existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			code, _, body := ts.get(t, tC.urlPath)
+			if code != tC.expectedCode {
+				t.Errorf("expected %d to be %d", code, tC.expectedCode)
+			}
+			if !bytes.Contains(body, tC.expectedBody) {
+				t.Errorf("expected %s to be %s", body, tC.expectedBody)
+			}
+		})
 	}
 }
