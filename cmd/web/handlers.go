d922655e9e5790d005880bdc59f74c36a1deecfe
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-26 11:46:29 +0200

    F add dummy handler and test

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -164,3 +164,7 @@ func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
 	app.session.Put(r, "flash", "You've been logget out successfully!")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ping(rr, r)
+
+	rs := rr.Result()
+
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("expected %d; got %d", http.StatusOK, rs.StatusCode)
+	}
+
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("Expected body to equal %q", "OK")
+	}
+}
