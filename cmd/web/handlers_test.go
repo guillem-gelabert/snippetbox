1d734aa957224b5e3221b7257b1ebceb3c129188
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-26 11:59:43 +0200

    t add end to end testing for the ping endpoint

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := &application{
+		errorLog: log.New(ioutil.Discard, "", 0),
+		infoLog:  log.New(ioutil.Discard, "", 0),
+	}
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	st := "./ui/static"
+	var static *string = &st
+
+	ts := httptest.NewTLSServer(app.routes(static))
+	defer ts.Close()
+
+	rs, err := ts.Client().Get(ts.URL + "/ping")
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	ping(rr, r)
-
-	rs := rr.Result()
 
 	if rs.StatusCode != http.StatusOK {
 		t.Errorf("expected %d; got %d", http.StatusOK, rs.StatusCode)
