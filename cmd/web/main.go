4fcc09b9e43c7cbcdaa008dfd08f74354619b301
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-07-27 15:09:05 +0200

    F update user model interface

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -35,6 +35,7 @@ type application struct {
 		Insert(string, string, string) error
 		Authenticate(string, string) (int, error)
 		Get(int) (*models.User, error)
+		ChangePassword(id int, currentPassword string, newPassword string) error
 	}
 }
 
